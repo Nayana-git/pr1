2b22d9e711a1e0b07555bfe126cf75fb80261602
nayana <nayana>
2022-10-22 02:50:02 -0400

    initail commit

---
+++ app/src/main/java/com/example/pr1/AccountDetail.kt
@@ -0,0 +1,11 @@
+package com.example.pr1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_detail)
+    }
+}
