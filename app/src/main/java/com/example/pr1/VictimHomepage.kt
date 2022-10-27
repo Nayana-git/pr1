ce68e0c3066ccea7b7fd97ca2728bd6be191b858
nayana <nayana>
2022-10-27 17:54:24 -0400

    Stats update

--- app/src/main/java/com/example/pr1/VictimHomepage.kt
+++ app/src/main/java/com/example/pr1/VictimHomepage.kt
@@ -8,15 +8,22 @@ import android.widget.Button
 class VictimHomepage : AppCompatActivity() {
 
     lateinit var btnFacebook: Button
+    lateinit var btnInstagram: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_victim_homepage)
 
         btnFacebook = findViewById(R.id.btnFacebook)
+        btnInstagram = findViewById(R.id.btnInstagram)
 
         btnFacebook.setOnClickListener {
             val intent = Intent(this, AccountDetail::class.java)
+            startActivity(intent)
+        }
+
+        btnInstagram.setOnClickListener {
+            val intent = Intent(this, VictimStats::class.java)
             startActivity(intent)
         }
     }
